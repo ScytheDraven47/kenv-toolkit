edbcf9c064929be15d84fff7b4b26d34e6aecafc
Ben Rogers-McKee <ben.rogers.mckee@gmail.com>
2023-05-17 12:07:49 +1200

    pushed from Script Kit

---
+++ scripts/new-mini-project.ts
@@ -0,0 +1,54 @@
+// Name: Create New Mini Project
+
+import '@johnlindquist/kit'
+
+const projectName = await arg({
+	placeholder: 'Project Name?',
+	onInput: (input) => {
+		setHint(formatProjectDirName(input))
+	},
+})
+
+const finalPath = home(
+	'OneDrive',
+	'Synced_Code',
+	'Projects',
+	formatProjectDirName(projectName)
+)
+const indexPath = `${finalPath}/index.html`
+const stylePath = `${finalPath}/style.css`
+const scriptPath = `${finalPath}/script.js`
+
+if (pathExists(finalPath)) {
+	const doContinue = await arg('Project already exists', [
+		{
+			name: 'Open',
+			value: true,
+		},
+		{
+			name: 'Pick a new name',
+			value: false,
+			onSubmit: async () => {
+				await run('new-mini-project')
+			},
+		},
+		{
+			name: 'Cancel',
+			value: false,
+		},
+	])
+	if (!doContinue) {
+		process.exit()
+	}
+}
+
+await ensureDir(finalPath)
+await ensureFile(indexPath)
+await ensureFile(stylePath)
+await ensureFile(scriptPath)
+
+await exec(`code ${indexPath} ${stylePath} ${scriptPath}`)
+
+function formatProjectDirName(projectName: string): string {
+	return projectName.toLowerCase().replaceAll(' ', '-')
+}
