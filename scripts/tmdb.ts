6765fcf05c19dd46adc4742372d25b4bf9df4828
Ben Rogers-McKee <ben.rogers.mckee@gmail.com>
2023-06-27 14:06:15 +1200

    added md step to TMDb script

--- scripts/tmdb.ts
+++ scripts/tmdb.ts
@@ -1,8 +1,22 @@
 // Name: The Movie Database
-// Shortcode:
+// Shortcode: tmdb
 
 import '@johnlindquist/kit'
 import { Choice } from '@johnlindquist/kit'
+
+const api_key = await env('TMDB_API_KEY')
+const TMDbUrl = (path = '', params = {}) => {
+	const url = new URL(`https://api.themoviedb.org/3${path}`)
+	const allParams = {
+		...Object.fromEntries(url.searchParams),
+		...params,
+		api_key,
+	}
+	url.search = new URLSearchParams(allParams).toString()
+	return url.toString()
+}
+const TMDbImagePath = (path: string) =>
+	`https://image.tmdb.org/t/p/original${path}`
 
 type TMDbSearchResult = {
 	adult: boolean
@@ -21,7 +35,71 @@ type TMDbSearchResult = {
 	vote_count: number
 }
 
-const api_key = await env('TMDB_API_KEY')
+type TMDbSeriesDetails = {
+	adult: boolean
+	backdrop_path: string
+	created_by: []
+	episode_run_time: number[]
+	first_air_date: string
+	genres: { id: number; name: string }[]
+	homepage: string
+	id: number
+	in_production: boolean
+	languages: string[]
+	last_air_date: string
+	last_episode_to_air: {
+		id: number
+		name: string
+		overview: string
+		vote_average: number
+		vote_count: number
+		air_date: string
+		episode_number: number
+		production_code: string
+		runtime: number
+		season_number: number
+		show_id: number
+		still_path: string
+	}
+	name: string
+	next_episode_to_air: null
+	networks: {
+		id: number
+		logo_path: string
+		name: string
+		origin_country: string
+	}[]
+	number_of_episodes: number
+	number_of_seasons: number
+	origin_country: ['JP']
+	original_language: string
+	original_name: string
+	overview: string
+	popularity: number
+	poster_path: string
+	production_companies: {
+		id: number
+		logo_path: string
+		name: string
+		origin_country: string
+	}[]
+	production_countries: { iso_3166_1: string; name: string }[]
+	seasons: {
+		air_date: string
+		episode_count: number
+		id: number
+		name: string
+		overview: string
+		poster_path: string
+		season_number: number
+	}[]
+	spoken_languages: { english_name: string; iso_639_1: string; name: string }[]
+	status: string
+	tagline: string
+	type: string
+	vote_average: number
+	vote_count: number
+}
 
 // const selectedMediaType = await arg('Movie or Series', ['Movie', 'Series'])
 
@@ -37,7 +115,6 @@ const selectedMedia = (await arg({
 
 		try {
 			const choices = await searchSeries(query)
-			debugger
 			setChoices(formatMediaChoices(choices))
 			setHint(choices.length === 0 ? 'No media found' : '')
 		} catch (error) {
@@ -50,48 +127,103 @@ const selectedMedia = (await arg({
 			name: 'Copy poster link',
 			bar: 'right',
 		},
+		backdrop: {
+			shortcut: 'ctrl+shift+enter',
+			name: 'Copy backdrop link',
+			bar: 'right',
+		},
 	},
 	height: 400,
 })) satisfies {
 	id: string
 	poster: string
+	backdrop: string
 }
 
 if (flag?.poster) {
 	await copy(selectedMedia.poster)
-} else {
-	// browse(`${selectedMedia.id}`)
+	exit()
+} else if (flag?.backdrop) {
+	await copy(selectedMedia.backdrop)
+	exit()
 }
+
+const seriesDetails = await getSeriesDetails(selectedMedia.id)
+
+await editor(formatMd(seriesDetails))
+// await editor(JSON.stringify(seriesDetails, null, 2))
 
 function formatMediaChoices(media: TMDbSearchResult[]): Choice[] {
 	return media.map((result) => {
 		let releaseYear = new Date(result.first_air_date).getFullYear()
-		let posterSrc = `https://image.tmdb.org/t/p/original${result.poster_path}`
+		let posterSrc = TMDbImagePath(result.poster_path)
+		let backdropSrc = TMDbImagePath(result.backdrop_path)
 		return {
 			id: result.id.toString(),
 			value: {
 				id: result.id.toString(),
 				poster: posterSrc,
+				backdrop: backdropSrc,
 			},
 			name: result.name,
 			description: `Release: ${releaseYear ?? '?'}`,
-			preview: `<img class="max-h-full max-w-full" src=${posterSrc} alt="Poster for ${result.name}">`,
+			preview: `<div class='grid grid-cols-5 gap-4 p-4'>
+				<img class='col-span-2' src=${posterSrc} alt="Poster for ${result.name}" />
+				<img class='col-span-3' src=${backdropSrc} alt="Backdrop for ${result.name}" />
+			</div>`,
 		} satisfies Choice
 	})
 }
 
+function formatMd(seriesDetails: TMDbSeriesDetails) {
+	let releaseYear = new Date(seriesDetails.first_air_date).getFullYear()
+	let posterSrc = TMDbImagePath(seriesDetails.poster_path)
+	let backdropSrc = TMDbImagePath(seriesDetails.backdrop_path)
+	return `---
+watched: 
+release: ${releaseYear}
+finished: ${seriesDetails.status === 'Ended' ? 'true' : 'false'}
+episode-length: ${seriesDetails.episode_run_time}
+episode-count:
+${seriesDetails.seasons
+	.filter((season) => season.season_number > 0)
+	.map((season) => ` - ${season.episode_count}`)
+	.join('\n')}
+genres:
+${seriesDetails.genres
+	.map((genre) => ` - ${genre.name.toLowerCase()}`)
+	.join('\n')}
+backdrop: ${backdropSrc}
+poster: ${posterSrc}
+---
+
+![|300](${backdropSrc})
+
+# ${seriesDetails.name}
+
+${seriesDetails.overview}
+
+![|300](${posterSrc})
+`
+}
+
 async function searchSeries(query) {
-	let response = await fetch(
-		`https://api.themoviedb.org/3/search/tv?api_key=${api_key}&query=${query}`
-	)
-	debugger
+	let response = await fetch(TMDbUrl('/search/tv', { query }))
 	let data = (await response.json()) satisfies {
 		page: number
 		results: TMDbSearchResult[]
 		total_pages: number
 		total_results: number
 	}
-	debugger
 
 	return data.results
+}
+
+async function getSeriesDetails(id) {
+	let response = await fetch(
+		TMDbUrl(`/tv/${id}`, { append_to_response: 'images' })
+	)
+	let data = (await response.json()) satisfies TMDbSeriesDetails
+
+	return data
 }
